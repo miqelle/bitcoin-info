e57eb221f11c7086522471b4dd464bcd2b2b38ae
miqelle <miqellec3@gmail.com>
2021-07-01 20:41:39 -0500

    added priceof bitcoin question prompt answer is 33,448.90

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ function myFunction() {
     var mylist = document.getElementById("myList");
     document.getElementById("demo").value = mylist.options[mylist.selectedIndex].text;
   }
+
+  var collective= true;
+  var question
+
+  while(collective==true){
+    question = prompt("Do you know the price of 1 bitcoin?")
+    if (question =="33,448.90") {collective = false;} 
+  }
+
