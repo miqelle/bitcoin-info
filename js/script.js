2d6e0c52ec8619fea5d7fdc0709108aed95579cd
miqelle <miqellec3@gmail.com>
2021-07-02 15:25:21 -0500

    display image equal to user input

--- js/script.js
+++ js/script.js
@@ -1,62 +1,21 @@
-function myFunction() {
-    var mylist = document.getElementById("myList");
-    document.getElementById("demo").value = mylist.options[mylist.selectedIndex].text;
-  }
+function askQuestion(){
+    var luckyNumberfound = false;
+    while(luckyNumberfound ==false){
+        var number = prompt("What is considered a lucky number?");
+        if(number == "7"){
+            alert("That's the lucky number!");
+            luckyNumberfound = true;
+        }
+        else{
+            alert("That's not a lucky number");
+        }
+    
+    }
+}    
 
-  var collective= true;
-  var question
+askQuestion();
 
-  while(collective==true){
-    question = prompt("Do you know the price of 1 bitcoin?")
-    if (question =="33,448.90") {collective = false;} 
-  }
-
-function validate(){
-
-//Create Fields Array
-
-var fields = new Array;
-
-var fields = [document.getElementById('username'),document.getElementById('pass')];
-
-//Create Variable to Keep Track of Errors
-
-var err = 0;
-
-
-
-//Start Validation Loop
-
-for (i=0;i<fields.length;i++){
-
-//Check Fields in Array to Make Sure they are not Empty
-
-if (fields[i].value == ""){
-
-err++;
-
+var amount = prompt("How many Bitcoins would you like?");
+for(let i = 0; i < amount; i++){
+    document.write("<img src='https://pbs.twimg.com/profile_images/586088541232431104/C7br1s6r_400x400.png'>"); 
 }
-
-}//Close Loop
-
-//Check That There are No Errors
-
-if (err === 0){
-
-//Submit Form
-
-document.myform.submit();
-
-}else {
-
-//If there are errors, return false and alert the user
-
-alert("Please Fill Out All Of The Fields");
-
-return false;
-
-}
-
-}
-
-
