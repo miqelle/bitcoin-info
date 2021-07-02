7df09eacbc8cd9651f00940961ecbe5216baa3a9
miqelle <miqellec3@gmail.com>
2021-07-02 08:20:57 -0500

    added validation for user  input

--- js/script.js
+++ js/script.js
@@ -11,3 +11,52 @@ function myFunction() {
     if (question =="33,448.90") {collective = false;} 
   }
 
+function validate(){
+
+//Create Fields Array
+
+var fields = new Array;
+
+var fields = [document.getElementById('username'),document.getElementById('pass')];
+
+//Create Variable to Keep Track of Errors
+
+var err = 0;
+
+
+
+//Start Validation Loop
+
+for (i=0;i<fields.length;i++){
+
+//Check Fields in Array to Make Sure they are not Empty
+
+if (fields[i].value == ""){
+
+err++;
+
+}
+
+}//Close Loop
+
+//Check That There are No Errors
+
+if (err === 0){
+
+//Submit Form
+
+document.myform.submit();
+
+}else {
+
+//If there are errors, return false and alert the user
+
+alert("Please Fill Out All Of The Fields");
+
+return false;
+
+}
+
+}
+
+
